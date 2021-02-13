748091bb0b252578913bcd7c16d20babb64ce3fa
alexandresantosm <amenezes031@gmail.com>
2021-02-13 12:29:35 -0300

    Application domain classes

---
+++ src/main/java/com/br/one/digitalinnovation/beerstock/entity/Beer.java
@@ -0,0 +1,41 @@
+package com.br.one.digitalinnovation.beerstock.entity;
+
+import com.br.one.digitalinnovation.beerstock.enums.BeerType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private int max;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BeerType type;
+}
