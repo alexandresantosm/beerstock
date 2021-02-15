1cb4eedbbfef746e7bd6767768e7de00bde6a9ab
alexandresantosm <amenezes031@gmail.com>
2021-02-14 21:58:20 -0300

    Interface BeerControllerDocs

---
+++ src/main/java/com/br/one/digitalinnovation/beerstock/controller/BeerControllerDocs.java
@@ -0,0 +1,19 @@
+package com.br.one.digitalinnovation.beerstock.controller;
+
+import com.br.one.digitalinnovation.beerstock.dto.BeerDTO;
+import com.br.one.digitalinnovation.beerstock.exception.BeerAlreadyRegisteredException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api("Manages beer stock")
+public interface BeerControllerDocs {
+
+    @ApiOperation(value = "Beer creation operation")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Success beer creation."),
+            @ApiResponse(code = 400, message = "Missing required fields or wrong field range value.")
+    })
+    BeerDTO createBeer(BeerDTO beerDTO) throws BeerAlreadyRegisteredException;
+}
