b732424ff48466f61246707ef08f1095b8d46b80
alexandresantosm <amenezes031@gmail.com>
2021-02-15 14:34:55 -0300

    Testing find beer by valid name

--- src/test/java/com/br/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
+++ src/test/java/com/br/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
@@ -73,7 +73,7 @@ public class BeerControllerTest {
     }
 
     @Test
-    void whenPOSTIsCalledWithoutRequiredFieldThenAnErrorIsReturn() throws Exception {
+    void whenPOSTIsCalledWithoutRequiredFieldThenAnErrorIsReturned() throws Exception {
         //given
         BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
         beerDTO.setBrand(null);
@@ -82,6 +82,24 @@ public class BeerControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(asJsonString(beerDTO)))
                 .andExpect(status().isBadRequest())
+        ;
+    }
+
+    @Test
+    void whenGETIsCalledWhitValidBeerNameThenAnBeerIsReturned() throws Exception {
+        // given
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+
+        // when
+        when(beerService.findByName(beerDTO.getName())).thenReturn(beerDTO);
+
+        // then
+        mockMvc.perform(get(BEER_API_URL_PATH + "/" + beerDTO.getName())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(beerDTO.getName())))
+                .andExpect(jsonPath("$.brand", is(beerDTO.getBrand())))
+                .andExpect(jsonPath("$.type", is(beerDTO.getType().toString())))
         ;
     }
 }
